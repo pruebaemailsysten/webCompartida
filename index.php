72d59406a9e8d7ac37bd4990fdc6b935d0becc2c
Saúl Doe <saulmm.infor@gmail.com>
2022-04-08 16:17:24 +0200

    Probando el commit

--- index.php
+++ index.php
@@ -4,9 +4,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/estructura.css">
+    <link rel="stylesheet" href="./css/estilo.css">
+    
     <title>Probando</title>
 </head>
 <body>
+
+    <header class="flex jcc ancho100">
+        <div class="flex jcc aic">
+            <img class="logo" src="./logo.png" alt="">
+        </div>
+
+        <nav>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+       
+
+    </header>
+
+
+
     <h1>Titulo añadido en un segundo conmmit</h1>
     <h2>Un subtitulo</h2>
 </body>
