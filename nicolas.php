5d8038d07056db281d0aca7a259a692a0b08e979
niko-os87 <nicolasorolsineiro@google.com>
2022-04-08 17:33:48 +0200

    añadido meta responsive a nicolas.php

--- nicolas.php
+++ nicolas.php
@@ -2,6 +2,7 @@
     <head>
         <title>niko</title>
         <meta charset="utf-8" lang="es">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./css/estructura_FResp.css">
     </head>
 
