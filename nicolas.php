a274e1fdacf58410d58740456d8bdd70b4066b7d
niko-os87 <nicolasorolsineiro@google.com>
2022-04-08 17:36:31 +0200

    añadido reset.css al head en nicolas.php

--- nicolas.php
+++ nicolas.php
@@ -3,6 +3,7 @@
         <title>niko</title>
         <meta charset="utf-8" lang="es">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/reset.css">
         <link rel="stylesheet" href="./css/estructura_FResp.css">
     </head>
 
