6a6eefb723c3e76713caf8932483a4f781272073
Lucas <fixako9339@procowork.com>
2022-04-08 18:14:30 +0200

    mod lucas (8)

--- lucas.php
+++ lucas.php
@@ -4,7 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style_lucas.css">
+    <script src="js/jquery.min.js"></script>
     <title>Página de Lucas</title>
+    <script>
+        $(function(){
+            $("#destroy").click(function(){
+                alert("It's all ogre now");
+                $ ("body").fadeOut(2000);
+                });
+            })
+    </script>
 </head>
 <body>
     <header class="w100 flex jcc aic">
@@ -18,14 +27,16 @@
             </div>
         </div>
     </header>
-    <div class="w100 flex jcc aic">
+    <div class="w100 flex jcc aic wrap">
         <p>Añadido unos minutos despues<b>Ayy lmao</b></p>
+        <div class="w100 flex jcc aic">
+            <button id="destroy">Destruir Pagina</button>
+        </div>
     </div>
 </body>
 <footer class="w100 flex jcc aic">
     <div class="w25 flex column aic">
         <h6>Copyright By Me, 2022</h6>
     </div>
-
 </footer>
 </html>
